66ca59ef8cd81ad2e52e8a8011602e14973cd5f4
BlueJayBird11 <bluejaybird2001@gmail.com>
2022-02-09 22:54:51 -0600

    master

---
+++ techshell.c
@@ -0,0 +1,130 @@
+//////////////////////////////////////////
+// Name: Jay Reich, Josiah Norman
+// Date: January 26, 2022
+// Descriptions: Programming Assignment #3
+//////////////////////////////////////////
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <string.h>
+
+#ifndef MAX_SIZE
+#define MAX_SIZE 256
+#endif
+#ifndef DEBUG
+#define DEBUG 1
+#endif
+
+int main(int argc, char *argv[])
+{
+    // if(argc != 2)
+    // {
+    //     printf("Usage: ./prog2 <dir>\n");
+    //     return 0;
+    // }
+    pid_t c1;
+    char cwd[MAX_SIZE];
+    getcwd(cwd, MAX_SIZE);
+
+    char str[MAX_SIZE];
+    // This is the token that is used to split the string
+    const char s[2] = " ";
+    char *token;
+    // Set us a string that is the equivalent of the user typing exit to leave 
+    // char exit[256];
+    // strcpy(exit, "exit\n");
+
+    while(1)
+    {
+        printf("$ ");
+        //Get the user's input
+        fgets(str, MAX_SIZE, stdin);
+
+        /* //These are some tests of debugging
+        scanf("%s", &str[0]);
+        printf(str);
+        printf("%s : %s = %i\n",str, exit, strcmp(str, exit));*/
+        
+        printf("Line read: %s", str);
+        token = strtok(str, s);
+        int count = 0;
+        printf("Token(s):\n");
+        // Go through every token using " " as the key to separate each
+        while(token != NULL)
+        {
+            if(count == 0)
+            {
+                //if the first token is cd, pwd, or exit, exicute the command and break loop
+                if(DEBUG)
+                {
+                    printf("str==pwd: %d\n", strcmp(token, "pwd\n"));
+                }
+                if(!strcmp(token, "cd\n"))
+                {
+                    //move cwd to 2nd arg
+                    // int change = chdir(argv[1]);
+                    // if (change == -1)
+                    // {
+                    //     printf("Can't chdir to %s\n", argv[1]);
+                    //     exit(1);
+                    // }
+                }
+                else if(!strcmp(token, "pwd\n"))
+                {
+                    printf("pwd: %s\n", cwd);
+                }
+                else if(!strcmp(token, "exit\n"))
+                {
+                    exit(0);
+                }
+                break;
+            }
+            printf(" %s", token);
+            token = strtok(NULL, s);
+            if(token != NULL)
+                printf("\n");
+            count++;
+        }
+        printf("%i token(s) read\n\n", count);
+    }    
+    return 0;
+
+    /*
+
+    // where the fork happens, everything in the if is what the child is doing
+    if ((c1 = fork()) == 0)
+    {
+        printf("Current working directory: %s\n", cwd);
+        printf("Executing ls %s --all -l --human-readable\n", argv[1]);
+        
+        int change = chdir(argv[1]);
+        if (change == -1)
+        {
+            printf("Can't chdir to %s\n", argv[1]);
+            exit(1);
+        }
+        //// All the comments here were me just trying to figure out how to use ls in c
+        // char* args[6] = {"ls", newcwd, "--all", "-l", "--human-readable", NULL};
+        fflush(stdout);
+        // execl(newcwd, "ls", "--all", "-l", "--human-readable", NULL);
+        execl("/bin/ls", "ls", "--all", "-l", "--human-readable", argv[1], NULL);
+        // execv(newcwd, args);
+        // execl("bin/ls", "ls", "-la", "-l", "-h", NULL);
+        // printf("Failed\n");
+        exit(1); // exit with 1 in case the line failed, which would be a error in the code
+    }
+
+    int status;
+    // wait for child to finish and get the exit status
+    waitpid(c1, &status, 0);
+    if (WIFEXITED(status))
+    {
+        const int es = WEXITSTATUS(status);
+        printf("Exit status: %d\n", es);
+    }
+    exit(0);
+    */
+}
